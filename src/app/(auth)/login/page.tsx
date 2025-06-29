21343d5391b4b8839699f1ed3463e113ba09030f
atharvasonawane <sonawaneatharva2004@gmail.com>
2025-06-30 01:17:32 +0530

    Logical grouping of routes

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+// This comes into logicaal grouping of routes because login comes into (auth) folder and hence /login can be hit directly
+// ie. directly use /login to go to login page
+// and no need to do /auth/login or etc
+
+
+const Login = () => {
+    return <h1>Login</h1>;
+}
+ 
+export default Login; 
