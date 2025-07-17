2a19dc6acafc0a162dbb15757c57e70ed8321fc2
atharvasonawane <sonawaneatharva2004@gmail.com>
2025-07-18 02:28:23 +0530

    Authentication using clerk

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -3,11 +3,20 @@
 // This will treat this file as a client component because everything is by default is a server component in next js.
 
 import {useState } from "react";
+import { SignedIn, useAuth, useUser } from "@clerk/nextjs";
 
 export const Counter = () => {
 
+    const {isLoaded, userId, sessionId, getToken} = useAuth();
+    const{isSignedIn,user} = useUser();
+
     console.log("Counter Component");
     const[count, setCount] = useState(0);
+
+    if(!isLoaded || !isSignedIn){
+        return null;
+    }
+
     return ( 
         <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
      );
--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -3,6 +3,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Navigation = () => {
     const pathname = usePathname();
@@ -11,6 +12,12 @@ const Navigation = () => {
             <Link href='/' className={ pathname === '/' ? "text-white-500 mr-4" : "text-blue-500 mr-4"}>Home</Link>
             <Link href='/about' className={pathname === '/about' ? "text-white-500 mr-4" : "text-blue-500 mr-4"}>About</Link>
             <Link href='/products/1' className={ pathname.startsWith("/products/1") ? "text-white-500 mr-4" : "text-blue-500 mr-4"}>Products</Link>
+            <SignedOut>
+                <SignInButton mode="modal"></SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton></UserButton>
+            </SignedIn>
         </nav>
      );
 }
