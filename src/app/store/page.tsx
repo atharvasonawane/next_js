7b0972aa99ce1a736e42097f45c3968d2e0cb3e3
atharvasonawane <sonawaneatharva2004@gmail.com>
2025-07-20 08:54:17 +0530

    add increment and decrement functionality

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -10,8 +10,20 @@ const App = () => {
     return <OtherComponent count={count}></OtherComponent>
 }
 
-const OtherComponent = ( {count}: {count:number} ) => {
-    return <div>{count}</div>
+const OtherComponent = ({ count }: { count: number }) => {
+    const incrementAsync = useCounterStore((state) => state.incrementAsync);
+    const decrement = useCounterStore((state) => state.decrement);
+
+    return (
+        <div>
+            {count}
+            <div>
+                <button onClick={incrementAsync}>Increment</button>
+                <button onClick={decrement}>Decrement</button>
+            </div>
+        </div>
+    )
+
 }
- 
+
 export default App;
